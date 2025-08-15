6fe8fe74f02faf19158dd2026cedb60c0af5d621
Bruno Lima <bl4_@hotmail.com>
2025-08-15 11:11:36 +0100

    Test new website

--- Test/my-site/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Test/my-site/",     // caminho público real no teu domínio
-  build: { outDir: "dist" },  // gera para dist (mais simples de gerir)
-});
