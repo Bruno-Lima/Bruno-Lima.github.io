3db3880ef8672d20e25820c2b98e4238e881b902
Bruno Lima <bl4_@hotmail.com>
2025-08-15 10:59:00 +0100

    new website test

--- Test/my-site/vite.config.js
+++ Test/my-site/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- // Se o repositório for "my-site", usa "/my-site/"
-  // Se for user site (repo "username.github.io"), usa "/"
+ // Your real hosting path:
   base: "/Test/my-site/",
-})
+  // Optional: output directly into that folder in your repo
+  build: {
+    outDir: "Test/my-site",
+    emptyOutDir: true, // careful: it will clear that folder on build
+  },
+});
